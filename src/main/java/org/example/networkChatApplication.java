269fca8bd5b0edf130359fef0627048ccd370ab4
codekim97 <kimtaeu4616@gmail.com>
2023-05-18 17:44:43 +0900

    수정

--- src/main/java/org/example/networkChatApplication.java
+++ src/main/java/org/example/networkChatApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NetworkChatApplication {
+public class networkChatApplication {
     public static void main(String[] args) {
-        SpringApplication.run(NetworkChatApplication.class, args);
+        SpringApplication.run(networkChatApplication.class, args);
     }
 }
